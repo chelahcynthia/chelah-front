842a0629b0ed6f9cce87c847911c02fa7ef896ce
chelahcynthia <cchelangat989@gmail.com>
2023-11-07 17:43:01 +0300

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+     
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <div className="flex w-full justify-between">
+                <SideBar />
+                <div className="grow h-[100vh]">
+                  <Navbar />
+                  <Outlet />
+                </div>
+              </div>
+            }
+          >
+            
+            <Route path="/home" element={<Home />} />
+          </Route>
+        </Routes>
+      
       
     </div>
   );
