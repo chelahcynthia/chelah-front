7e810577eab3db0d0c9753c0cfccdf1d5ff41729
chelahcynthia <cchelangat989@gmail.com>
2023-11-08 07:15:52 +0300

    Import img

--- src/components/dashboard/SideBar.js
+++ src/components/dashboard/SideBar.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import realestate from "../../assets/realestate.png";
 
 const SideBar = () => {
   return (
-    <aside>
-      
+    <aside className="h-screen">
+      <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+        <div className="p-4 pb-2 flex justify-between items-center">
+          <img 
+          src={realestate} 
+          className={`overflow-hidden transition-all ${
+            expanded ? "w-32" : "w-0"
+          }`}
+          alt=""
+          />
+
+        </div>
+
+
+      </nav>
+
     </aside>
   )
 }
