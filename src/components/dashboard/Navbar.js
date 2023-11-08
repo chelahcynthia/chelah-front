79ce6a2446a3db26b36d2d1856dfe02161ff859f
chelahcynthia <cchelangat989@gmail.com>
2023-11-08 14:50:40 +0300

    Navbar fix

--- src/components/dashboard/Navbar.js
+++ src/components/dashboard/Navbar.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
+import ham from "../../icons/ham.png";
+import logoutIcon from "../../icons/logout.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-      <p>Don't worry , I will get done with the navbar in a few!</p>
-
+    <div className='navbar'>
+    <div className="subnavbar">
+      <div className="leftnav">
+        <img src={ham} className='navbarIcons' alt=" " />
+        <p style={{color:"white" ,marginLeft:"14px", marginTop:12}}> Hello Admin</p>
+      </div>
+      <div className="cursor-pointer ">
+        <img src={logoutIcon} className='navbarIcons' alt='' />
+      </div>
     </div>
-  )
-}
+   </div>
+  );
+};
 
-export default Navbar
+
+
+export default Navbar;
